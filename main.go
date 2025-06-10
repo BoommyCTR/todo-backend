422b93b6d84e0ced444da1c1eb92d0bffeead639
BoomCTR <boommy.ctr@gmail.com>
2025-06-10 15:48:08 +0700

    add new url to cors

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173, http://127.0.0.1:5173",
+		AllowOrigins:     "http://localhost:5173, http://127.0.0.1:5173, http://localhost:4173, http://127.0.0.1:4173",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
